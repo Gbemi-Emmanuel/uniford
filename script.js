0d7aa077ef8ec3bb2d9122b68978e1384aa113ae
Gbemi-Emmanuel <109102914+Gbemi-Emmanuel@users.noreply.github.com>
2022-08-11 16:26:55 +0100

    updated

--- script.js
+++
@@ -1,8 +0,0 @@
-var navLinks = document.getElementById("navLinks");
-
-function openMenu(){
-    navLinks.style.right = "0";
-}
-function closeMenu(){
-    navLinks.style.right = "-200px";
-}
---
+++ script/script.js
@@ -0,0 +1,8 @@
+var navLinks = document.getElementById("navLinks");
+
+function openMenu(){
+    navLinks.style.right = "0";
+}
+function closeMenu(){
+    navLinks.style.right = "-200px";
+}
