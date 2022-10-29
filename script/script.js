668af10cf31427ad8aa1225c392e8dab3e8adb3a
Gbemi-Emmanuel <109102914+Gbemi-Emmanuel@users.noreply.github.com>
2022-10-29 11:20:29 +0100

    made responsive

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ var navLinks = document.getElementById("navLinks");
 
 function openMenu(){
     navLinks.style.right = "0";
+    navLinks.style.display = "block";
 }
 function closeMenu(){
     navLinks.style.right = "-200px";
